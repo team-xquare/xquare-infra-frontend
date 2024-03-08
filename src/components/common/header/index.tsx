715d6be63cb6e22b852398616e7f85dba73fc83d
dutexion <dutexion@gmail.com>
2024-03-09 03:50:03 +0900

    feat :: scroll에 따른 header 임시

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,19 +1,43 @@
 import { Outlet } from 'react-router-dom';
 import styled from '@emotion/styled';
+import LogoImg from '@/assets/Logo.svg';
+import { useEffect, useState } from 'react';
+
+// 0 408
 
 export const Header = () => {
+  const [scroll, setScroll] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.pageYOffset || document.documentElement.scrollTop);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Wrapper>
-        xquare<div>asd</div>zxc
+      <Wrapper scroll={scroll}>
+        <img src={LogoImg} />
+        <div>
+          <span>소개</span>
+          <span>고객 지원</span>
+        </div>
+        <Button></Button>
       </Wrapper>
       <Outlet />
     </>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ scroll: number }>`
   position: fixed;
+  transition: 0.2s linear;
   top: 0;
   left: 0;
   width: 100%;
@@ -22,4 +46,19 @@ const Wrapper = styled.div`
   justify-content: space-between;
   padding: 0 90px 0 90px;
   align-items: center;
+  color: ${({ scroll }) => (scroll === 0 ? 'white' : scroll >= 408 ? 'rgba(0,0,0,0)' : 'black')};
+  background-color: ${({ scroll }) => (scroll === 0 ? 'rgba(0,0,0,0)' : scroll >= 408 ? 'rgba(0,0,0,0)' : 'white')};
+  z-index: 999;
+  > div {
+    width: 254px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Button = styled.div`
+  width: 192px;
+  height: 52px;
+  border-radius: 8px;
+  border: 2px white solid;
 `;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,20 @@
 import styled from '@emotion/styled';
+import MainImg from '@/assets/Main.svg';
 
 export const Main = () => {
   return (
     <Wrapper>
-      <Container>a</Container>
+      <Container>
+        <img src={MainImg} />
+        <FirstContainerBox>
+          <div>
+            프로젝트를 빠르게
+            <br />
+            배포하는 경험을 할 수 있습니다.
+          </div>
+          <div>스퀘어는 보다 쉬운 방법으로 배포할 수 있고 쉽게 모니터링 할 수 있습니다.</div>
+        </FirstContainerBox>
+      </Container>
       <Container>b</Container>
     </Wrapper>
   );
@@ -17,4 +28,28 @@ const Wrapper = styled.div`
 const Container = styled.div`
   width: 100%;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+const FirstContainerBox = styled.div`
+  position: absolute;
+  top: 200px;
+  left: 214px;
+  width: 922px;
+  height: 690px;
+  color: white;
+  > div:nth-child(1) {
+    font-size: 64px;
+    font-weight: 800;
+    margin-top: 20px;
+  }
+  > div:nth-child(2) {
+    font-size: 30px;
+    font-weight: 500;
+    margin-top: 50px;
+  }
 `;
