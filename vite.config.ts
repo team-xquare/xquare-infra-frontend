7947ca72107e73d0dfaeda471794194c227f3d88
dutexion <dutexion@gmail.com>
2024-03-08 16:53:33 +0900

    setting :: svg 세팅 및 경로 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
 
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
-})
+	plugins: [react(), svgrPlugin()],
+	server: {
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
