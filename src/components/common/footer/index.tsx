d9864c319122ec48eb007c246f6c9018233d8893
dutexion <dutexion@gmail.com>
2024-03-12 20:14:31 +0900

    design :: footer 틀

---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Footer = () => {
+  return <Wrapper></Wrapper>;
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 544px;
+  background-color: #17181a;
+`;
