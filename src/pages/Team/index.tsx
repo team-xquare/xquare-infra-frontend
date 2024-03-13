da85c4ec75e0659bfecad1b695f18f59b1103dc2
dutexion <dutexion@gmail.com>
2024-03-13 18:48:40 +0900

    design :: team page 틀

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,10 +1,23 @@
+import { Sidebar } from '@/components/common/sidebar';
 import styled from '@emotion/styled';
 
 export const Team = () => {
-  return <Wrapper>hello</Wrapper>;
+  return (
+    <Wrapper>
+      <Sidebar />
+      <Container>hello</Container>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
   margin-top: 80px;
+  margin-left: 60px;
   width: 100%;
+  display: flex;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vw;
 `;
