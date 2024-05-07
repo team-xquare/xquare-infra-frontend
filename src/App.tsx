3594cc704396df8840d1d36678ffbba79f2299b5
dutexion <dutexion@gmail.com>
2024-05-07 15:39:15 +0900

    chore :: app console del

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { Router } from './router/router';
 import { StyledProvider } from './style/StyledProvider';
 
 export const App = () => {
-  console.log(import.meta.env.VITE_SERVER_BASE_URL);
-
   return (
     <StyledProvider>
-      last
       <RouterProvider router={Router} />
     </StyledProvider>
   );
