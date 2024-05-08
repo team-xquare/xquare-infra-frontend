43a40ba8118eee659e5de649649e7317871b488d
dutexion <dutexion@gmail.com>
2024-05-08 11:11:01 +0900

    feat :: team api code

--- src/utils/apis/team.ts
+++ src/utils/apis/team.ts
@@ -10,3 +10,21 @@ export const teamCheck = async () => {
 export const teamCreate = async (data: TeamCreateType) => {
   return await instance.post(`${router}`, data);
 };
+
+export const teamDetailCheck = async (uuid: string) => {
+  return await instance.get(`${router}/detail/${uuid}`);
+};
+
+export const teamMemberPut = async (uuid: string, user_ids: string[]) => {
+  return await instance.put(`${router}/${uuid}/members`, {
+    members: user_ids,
+  });
+};
+
+export const teamMemberDelete = async (uuid: string, user_id: string) => {
+  return await instance.delete(`${router}/${uuid}/members`, {
+    data: {
+      user_id: user_id,
+    },
+  });
+};
