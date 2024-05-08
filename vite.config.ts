a7ed453d6bb837617150483f0461cdb22628292a
dutexion <dutexion@gmail.com>
2024-05-09 08:20:49 +0900

    setting :: chart js

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import * as path from "path";
+import * as path from 'path';
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import svgrPlugin from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-	plugins: [react(), svgrPlugin()],
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react(), svgrPlugin()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['chartjs-adapter-date-fns'],
+  },
 });
