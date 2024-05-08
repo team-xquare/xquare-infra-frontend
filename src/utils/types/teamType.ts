1c6a83a567f5fd44d0afd4adf455410fa78af93d
dutexion <dutexion@gmail.com>
2024-05-08 10:00:05 +0900

    type :: team Type

--- src/utils/types/teamType.ts
+++ src/utils/types/teamType.ts
@@ -1,8 +1,38 @@
 export type TeamType = 'CLUB' | 'TEAM_PROJECT' | 'PRIVATE_PROJECT' | 'ETC' | string;
+export type MemberRoleType = 'ADMINISTRATOR' | 'MEMBER';
+
+export type TeamListType = {
+  team_id: string;
+  team_name_ko: string;
+  team_name_en: string;
+  administrator_name: string;
+  team_type: TeamType;
+  deploy_list: string[];
+};
+
+export type TeamCheckType = {
+  team_list: TeamListType[];
+};
 
 export type TeamCreateType = {
   team_name_ko: string;
   team_name_en: string;
   team_type: TeamType;
   team_member_list: string[];
+};
+
+export type MemberType = {
+  member_name: string;
+  member_number: string;
+  member_role: MemberRoleType[];
+  user_id: string;
+};
+
+export type TeamDetailType = {
+  team_name_ko: string;
+  team_name_en: string;
+  member_count: number;
+  admin_name: string;
+  created_at: string;
+  member_list: MemberType[];
 };
