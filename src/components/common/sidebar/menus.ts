691ec802fee7248125851dc453be13a8269a2732
dutexion <dutexion@gmail.com>
2024-06-17 22:26:52 +0900

    chore :: 경로 수정

--- src/components/common/sidebar/menus.ts
+++ src/components/common/sidebar/menus.ts
@@ -17,10 +17,10 @@ const team = {
 };
 
 const deploy = {
-  back: '/team/-team-/manage',
+  back: '/team',
   menu: [
-    { icon: 'material-symbols:file-copy-outline', name: '배포', link: '/team/-team-/deploy' },
-    { icon: 'f7:shippingbox', name: '컨테이너', link: `/team/-team-/container` },
+    { icon: 'ph:circles-four-light', name: '팀 관리', link: '/team/-team-/manage' },
+    { icon: 'f7:shippingbox', name: '배포', link: `/team/-team-/deploy` },
   ],
 };
 
@@ -35,16 +35,10 @@ const deploy_inside = {
 export const menu: MenusType = {
   '/team': team,
   '/team/create': team,
-  '/team/:id/manage': {
-    back: '/team',
-    menu: [
-      { icon: 'ph:circles-four-light', name: '팀 관리', link: '/team/-team-/manage' },
-      { icon: 'f7:shippingbox', name: '배포', link: `/team/-team-/deploy` },
-    ],
-  },
+  '/team/:id/manage': deploy,
   '/team/:id/deploy': deploy,
   '/team/:id/deploy/create': deploy,
-  '/team/:id/container': deploy,
+  // '/team/:id/container': deploy,
   '/team/:id/deploy/:id': deploy_inside,
   '/team/:id/deploy/:id/container': deploy_inside,
   '/team/:id/deploy/:id/container/:env': {
