15e0d5ddfda7f40cf57623b3e2a9594c53799f6c
dutexion <dutexion@gmail.com>
2024-06-19 22:23:21 +0900

    feat :: github api

---
+++ src/utils/apis/github.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError } from 'axios';
+
+function isAxiosError(error: unknown): error is AxiosError {
+  return (error as AxiosError).isAxiosError !== undefined;
+}
+
+export const checkFileExists = async (repoUrl: string | undefined, branch: string, filePath: string) => {
+  const url = `https://api.github.com/repos/${repoUrl}/contents/${filePath}?ref=${branch}`;
+  try {
+    await axios.get(url, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    });
+    return true;
+  } catch (error) {
+    if (isAxiosError(error) && error.response && error.response.status === 404) {
+      return false;
+    } else {
+      throw error;
+    }
+  }
+};
