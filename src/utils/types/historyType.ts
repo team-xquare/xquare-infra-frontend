08e2208b4214bab5f334cbee64b2956df64f14a9
dutexion <dutexion@gmail.com>
2024-08-01 19:49:20 +0900

    create :: history type

---
+++ src/utils/types/historyType.ts
@@ -0,0 +1,12 @@
+export type StageType = {
+  name: 'build' | 'deploy';
+  status: 'Building' | 'Passed' | 'Failed' | 'Unknown';
+};
+
+export type HistoryType = {
+  name: string;
+  email: string;
+  scheduled_date: number;
+  commit_message: string;
+  stages: StageType[];
+};
