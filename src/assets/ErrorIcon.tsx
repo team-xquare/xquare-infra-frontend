ef49263dcf468e4bfeffdc0d0412943ac9bae033
dutexion <dutexion@gmail.com>
2024-08-06 23:16:51 +0900

    create :: error icon

---
+++ src/assets/ErrorIcon.tsx
@@ -0,0 +1,12 @@
+import { IconPropsType } from './IconType';
+
+export const ErrorIcon = ({ size = 24, color = 'currentColor' }: IconPropsType) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24">
+      <path
+        fill={color}
+        d="M13 13h-2V7h2m0 10h-2v-2h2M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+      />
+    </svg>
+  );
+};
