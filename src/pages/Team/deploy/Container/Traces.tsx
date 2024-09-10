70c6d2506bdca32e3bf87fe410e7e27012e20b6d
dutexion <dutexion@gmail.com>
2024-09-10 12:00:38 +0900

    chore :: prettier

--- src/pages/Team/deploy/Container/Traces.tsx
+++ src/pages/Team/deploy/Container/Traces.tsx
@@ -147,9 +147,7 @@ export const TeamDeployContainerTraces = () => {
                     <MethodItem>{trace.method ?? ''}</MethodItem>
                     <StatusCodeItem>
                       {trace.status_code && (
-                        <StatusCodeBox status={trace.status_code}>
-                          {trace.status_code}
-                        </StatusCodeBox>
+                        <StatusCodeBox status={trace.status_code}>{trace.status_code}</StatusCodeBox>
                       )}
                     </StatusCodeItem>
                   </TraceItem>
@@ -163,197 +161,196 @@ export const TeamDeployContainerTraces = () => {
 };
 
 const Wrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
 `;
 
 const TracesContainer = styled.div`
-    width: 100%;
-    max-width: 1200px;
-    border-radius: 8px;
-    border: 1px solid ${theme.color.gray4};
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
+  width: 100%;
+  max-width: 1200px;
+  border-radius: 8px;
+  border: 1px solid ${theme.color.gray4};
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 `;
 
 const TraceLabel = styled.div`
-    width: 100%;
-    color: ${theme.color.gray8};
-    font-size: 16px;
-    font-weight: 600;
-    display: flex;
-    height: 52px;
-    background-color: ${theme.color.gray2};
-    border-bottom: 1px solid ${theme.color.gray4};
+  width: 100%;
+  color: ${theme.color.gray8};
+  font-size: 16px;
+  font-weight: 600;
+  display: flex;
+  height: 52px;
+  background-color: ${theme.color.gray2};
+  border-bottom: 1px solid ${theme.color.gray4};
 `;
 
 const ScrollableTraceItems = styled.div`
-    max-height: calc(100vh - 300px);
-    overflow-y: auto;
-    overflow-x: hidden;
+  max-height: calc(100vh - 300px);
+  overflow-y: auto;
+  overflow-x: hidden;
 `;
 
 const TraceItem = styled.div`
-    width: 100%;
-    color: ${theme.color.gray8};
-    font-size: 16px;
-    font-weight: 300;
-    display: flex;
-    height: 30px;
-    border-bottom: 1px solid ${theme.color.gray4};
-    background-color: ${theme.color.gray1};
-    transition: 0.1s linear;
-    cursor: pointer;
-    &:last-child {
-        border-bottom: none;
-    }
-    &:hover {
-        background-color: #eaf6fc;
-    }
+  width: 100%;
+  color: ${theme.color.gray8};
+  font-size: 16px;
+  font-weight: 300;
+  display: flex;
+  height: 30px;
+  border-bottom: 1px solid ${theme.color.gray4};
+  background-color: ${theme.color.gray1};
+  transition: 0.1s linear;
+  cursor: pointer;
+  &:last-child {
+    border-bottom: none;
+  }
+  &:hover {
+    background-color: #eaf6fc;
+  }
 `;
 
 const DateLabel = styled.div`
-    width: 25%;
-    min-width: 200px;
-    padding-left: 20px;
-    display: flex;
-    align-items: center;
+  width: 25%;
+  min-width: 200px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
 `;
 
 const ResourceLabel = styled.div`
-    width: 35%;
-    min-width: 250px;
-    display: flex;
-    align-items: center;
+  width: 35%;
+  min-width: 250px;
+  display: flex;
+  align-items: center;
 `;
 
 const DurationLabel = styled.div`
-    width: 10%;
-    min-width: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 10%;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const MethodLabel = styled.div`
-    width: 10%;
-    min-width: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 10%;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StatusCodeLabel = styled.div`
-    width: 20%;
-    min-width: 150px;
-    padding-right: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 20%;
+  min-width: 150px;
+  padding-right: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const DateItem = styled.div`
-    width: 25%;
-    min-width: 200px;
-    padding-left: 20px;
-    display: flex;
-    align-items: center;
-    font-variant-numeric: tabular-nums;
+  width: 25%;
+  min-width: 200px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+  font-variant-numeric: tabular-nums;
 `;
 
 const ResourceItem = styled.div`
-    width: 35%;
-    min-width: 250px;
-    display: flex;
-    align-items: center;
-    font-variant-numeric: tabular-nums;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+  width: 35%;
+  min-width: 250px;
+  display: flex;
+  align-items: center;
+  font-variant-numeric: tabular-nums;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 const DurationItem = styled.div`
-    width: 10%;
-    min-width: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-variant-numeric: tabular-nums;
+  width: 10%;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-variant-numeric: tabular-nums;
 `;
 
 const MethodItem = styled.div`
-    width: 10%;
-    min-width: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 10%;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StatusCodeItem = styled.div`
-    width: 20%;
-    min-width: 150px;
-    padding-right: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-variant-numeric: tabular-nums;
+  width: 20%;
+  min-width: 150px;
+  padding-right: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-variant-numeric: tabular-nums;
 `;
 
 const GraphContainer = styled.div`
-    width: 100%;
-    max-width: 1300px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    gap: 20px;
+  width: 100%;
+  max-width: 1300px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px;
 
-    @media (max-width: 1300px) {
-        justify-content: center;
-    }
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
 `;
 
 const GraphBox = styled.div`
-    flex: 1;
-    min-width: 370px;
-    max-width: 370px;
-    padding: 10px 20px;
-    border-radius: 6px;
-    border: 1px solid ${theme.color.gray4};
-    background-color: ${theme.color.gray1};
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: start;
-    > span {
-        font-size: 20px;
-        font-weight: 500;
-        color: ${theme.color.gray8};
-    }
+  flex: 1;
+  min-width: 370px;
+  max-width: 370px;
+  padding: 10px 20px;
+  border-radius: 6px;
+  border: 1px solid ${theme.color.gray4};
+  background-color: ${theme.color.gray1};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  > span {
+    font-size: 20px;
+    font-weight: 500;
+    color: ${theme.color.gray8};
+  }
 
-    @media (max-width: 460px) {
-        min-width: 100%;
-    }
+  @media (max-width: 460px) {
+    min-width: 100%;
+  }
 `;
 
 const StatusCodeBox = styled.div<{ status: number }>`
   padding: 2px 8px;
   border-radius: 4px;
   font-weight: 220;
-  color: ${props => {
-      if (props.status < 300) return theme.color.gray1;
-      if (props.status < 400) return theme.color.gray1;
-      if (props.status < 500) return theme.color.gray1;
-      return theme.color.gray1;
-}};
-  background-color: ${props => {
-  
-      if (props.status < 300) return theme.color.green;
-      if (props.status < 400) return theme.color.infoDark1;
-      if (props.status < 500) return theme.color.brown;
-      return theme.color.red;
-}};
+  color: ${(props) => {
+    if (props.status < 300) return theme.color.gray1;
+    if (props.status < 400) return theme.color.gray1;
+    if (props.status < 500) return theme.color.gray1;
+    return theme.color.gray1;
+  }};
+  background-color: ${(props) => {
+    if (props.status < 300) return theme.color.green;
+    if (props.status < 400) return theme.color.infoDark1;
+    if (props.status < 500) return theme.color.brown;
+    return theme.color.red;
+  }};
 `;
