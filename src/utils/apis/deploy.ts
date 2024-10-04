9cd7fb458c93877cbe04f6fd638444ae8cf02fd7
dutexion <dutexion@gmail.com>
2024-10-04 18:23:00 +0900

    fix :: deploy error

--- src/utils/apis/deploy.ts
+++ src/utils/apis/deploy.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { DeployAllType, DeployCreateType } from '../types/deployType';
 import { instance } from './axios';
 
